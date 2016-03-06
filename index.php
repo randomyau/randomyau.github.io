05fee1d4769d2e55ce5c393a7c27c6fa36d2ce07
randomyau <210078@hkis.edu.hk>
2016-03-06 14:14:44 +0800

    tried contact

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: TangledDemo'; 
+    $to = '210078@hkis.edu.hk'; 
+    $subject = 'Dis from the contact form';
+    $human = $_POST['human'];
+			
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+				
+    if ($_POST['submit'] && $human == '4') {				 
+        if (mail ($to, $subject, $body, $from)) { 
+	    echo '<p>Your message has been sent!</p>';
+	} else { 
+	    echo '<p>Something went wrong, go back and try again!</p>'; 
+	} 
+    } else if ($_POST['submit'] && $human != '4') {
+	echo '<p>You don’t know what 2+2 is?x</p>';
+    }
+?>
